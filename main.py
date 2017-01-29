745eb7ff6e8836bd5a53d91eb13a04ef14f9c1b9
Daniel Beckwith <djbsnx@gmail.com>
2017-01-29 13:40:16 -0500

    Cleans up trail algorithm.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import config
 from visualizer import Visualizer
 
 
+def trail_pos(t):
+    return int(np.clip((np.sin(t * 2 * np.pi / 4) + 1) / 2 * config.NUM_LEDS, 0, config.NUM_LEDS-1))
+    # return int(np.clip(2 * abs((t % 4) / 4 * config.NUM_LEDS - config.NUM_LEDS / 2), 0, config.NUM_LEDS-1))
+
 TAIL_LEN = 0.5
 FPS_PRINT_INTERVAL = 5.0
 
@@ -21,7 +25,6 @@ if __name__ == '__main__':
         start_frames = 0
         frames = 0
         offsets = np.array([0, -TAIL_LEN*1.5, -TAIL_LEN*3.0], dtype=np.float32)
-        xs = ([], [], [])
         signal.signal(signal.SIGINT, lambda *args: vis.stop())
         while not vis.stopped:
             t = time.time()
@@ -29,21 +32,19 @@ if __name__ == '__main__':
 
             pixels = np.zeros((config.NUM_LEDS, config.CHANNELS), dtype=np.float32)
             for c in range(config.CHANNELS):
-                pos = int(np.clip(int((np.sin((t + offsets[c]) * 2 * np.pi / 4) + 1) / 2 * config.NUM_LEDS), 0, config.NUM_LEDS-1))
-                xs[c].insert(0, (pos, t))
-                while t - xs[c][-1][1] >= TAIL_LEN:
-                    xs[c].pop()
-                if xs[c][0][0] <= xs[c][-1][0]:
-                    min_x = xs[c][0][0]
-                    max_x = xs[c][-1][0]
+                head = trail_pos(t + offsets[c])
+                tail = trail_pos(t + offsets[c] - TAIL_LEN)
+                if head <= tail:
+                    min_x = head
+                    max_x = tail
                     min_fade = 1.0
                     max_fade = 0.0
                 else:
-                    min_x = xs[c][-1][0]
-                    max_x = xs[c][0][0]
+                    min_x = tail
+                    max_x = head
                     min_fade = 0.0
                     max_fade = 1.0
-                pixels[min_x:max_x+1, c] = np.linspace(min_fade, max_fade, num=max_x+1-min_x) ** 5
+                pixels[min_x:max_x+1, c] = np.linspace(min_fade, max_fade, num=max_x+1-min_x) ** 1
             vis.send_pixels(pixels * 1.0)
 
             frames += 1

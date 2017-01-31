Mostly fixes scaling problem, adds spectrogram vis, remaps colors.

audio.py
@@ -113,13 +113,14 @@ class Audio(object):
 
         # TODO: breaks on test signal, probably some samples lengths cause problems, should custom implement
         frames = np.lib.stride_tricks.as_strided(samples, shape=(frame_count, frame_size), strides=(samples.strides[0] * hop_size, samples.strides[0]), writeable=False).copy()
-        
+
         window = np.hamming(frame_size)
         frames *= window
         spec = np.abs(np.fft.rfft(frames, n=frame_size * 2 - 1))
         print(np.min(spec), np.mean(spec), np.max(spec))
 
         time_bins, freq_bins = spec.shape
+        # TODO: may need to adjust freq scale
         freq_scale = np.linspace(0, 1, freq_bins, endpoint=False) ** 20
         # print(freq_scale)
         freq_scale *= freq_bins - 1
@@ -138,27 +139,31 @@ class Audio(object):
         # spec = np.log10(spec)
         print(np.min(spec), np.mean(spec), np.max(spec))
         spec = util.lerp(spec, np.percentile(spec, 5), np.percentile(spec, 95), 0, 1)
-        print(np.min(spec), np.mean(spec), np.max(spec)) # TODO: final scale is pretty low for Such Great Heights?
+        print(np.min(spec), np.mean(spec), np.max(spec))
         np.clip(spec, 0, 1, out=spec)
 
-        power_scale_min = 0.2
-        power_scale_max = 1
-        power_scale_power = 1
+        power_scale_min = 0.1
+        power_scale_max = 2
+        power_scale_power = 3
         power_scale = util.lerp(np.linspace(0, 1, spec.shape[1]) ** power_scale_power, 0, 1, power_scale_min, power_scale_max)
+        # TODO: would be nice to have some kind of debug window showing these curves
+        # from matplotlib import pyplot as plt
+        # plt.plot(power_scale)
+        # plt.show()
         spec *= power_scale
 
-        spec **= 1 / 1
+        spec **= 1 / 2
+        print(np.histogram(spec, density=True))
 
         # spec *= 10
 
-        util.gaussian_filter1d(spec, sigma=1, axis=0, output=spec)
-        util.gaussian_filter1d(spec, sigma=-0.5, axis=1, output=spec)
+        util.gaussian_filter1d(spec, sigma=1, axis=0, output=spec) # blur time axis
+        # util.gaussian_filter1d(spec, sigma=0.2, axis=1, output=spec) # blur freq axis
 
         self._spectrogram = spec
         # from matplotlib import pyplot as plt
-        # plt.imshow(self._spectrogram[:1000], cmap='gray')
+        # plt.imshow(self._spectrogram[:1000], cmap='gray', vmin=0.0, vmax=1.0)
         # plt.show()
-        # exit()
 
     @smooth(alpha_decay=0.2, alpha_rise=0.99)
     def spectrogram(self, t):

main.py
@@ -21,6 +21,15 @@ def resample(x, n):
         coord_from = coords[i]
         coord_to = coords[i + 1]
 
+def split_spec(spec):
+    ranges = np.linspace(0, spec.shape[0], config.NUM_LED_CHANNELS + 1, dtype=np.int_)
+    for i in range(config.NUM_LED_CHANNELS):
+        spec_from = ranges[i]
+        spec_to = ranges[i + 1]
+        spec_range = spec_to - spec_from
+        channel_vals = resample(spec[spec_from:spec_to], config.NUM_LEDS // 2)
+        yield np.concatenate((channel_vals[::-1], channel_vals))
+
 
 TAIL_LEN = 0.5
 FPS_PRINT_INTERVAL = 5.0
@@ -63,13 +72,12 @@ if __name__ == '__main__':
             while vis.running:
                 pixels = np.zeros((config.NUM_LEDS, config.NUM_LED_CHANNELS), dtype=np.float64)
                 spec = audio.spectrogram(audio.elapsed_time)
-                ranges = np.linspace(0, spec.shape[0], config.NUM_LED_CHANNELS + 1, dtype=np.int_)
-                for i in range(config.NUM_LED_CHANNELS):
-                    spec_from = ranges[i]
-                    spec_to = ranges[i + 1]
-                    spec_range = spec_to - spec_from
-                    channel_vals = resample(spec[spec_from:spec_to], config.NUM_LEDS // 2)
-                    pixels[:, i] = np.concatenate((channel_vals[::-1], channel_vals))
+                vis.update_spec(spec)
+                hi, med, low = tuple(split_spec(spec))
+                # TODO: map to other hues besides RGB? (might need to be linearly independent)
+                pixels[:, 0] = low
+                pixels[:, 1] = hi
+                pixels[:, 2] = med
 
                 vis.send_pixels(pixels)
 